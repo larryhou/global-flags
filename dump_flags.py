cf1b2b0e51992882b1ff54911af391abe74073c0
larryhou <larryhou@tencent.com>
2019-02-11 15:15:40 +0800

    Update

--- dump_flags.py
+++ dump_flags.py
@@ -45,18 +45,41 @@ class FlagsGenerator(object):
         data.sort(key=itemgetter('id'))
         return data
 
+def dump_missing(item_list, check_list):
+    check_map = {}
+    for it in check_list:
+        check_map[it] = it
+    missing_list = []
+    for it in item_list:
+        if it not in check_map: missing_list.append(it)
+    print(missing_list)
+
 def main():
     flags = FlagsGenerator()
     foreign_data = flags.load(url='http://flagpedia.net')
     flags = FlagsGenerator()
     chinese_data = flags.load(url='http://flagpedia.asia')
+    show_list = ['匈牙利', '希腊', '意大利', '列支敦斯登', '土耳其', '圣马力诺', '英国', '挪威', '爱尔兰岛', '爱沙尼亚', '丹麦', '喬治亞', '阿塞拜疆',
+                 '阿尔巴尼亚', '克罗地亚', '乌克兰', '羅馬尼亞', '瑞典', '瑞士', '塞尔维亚', '賽普勒斯', '葡萄牙', '卢森堡', '奥地利', '摩尔多瓦', '摩纳哥', '马耳他',
+                 '捷克', '荷兰', '白俄罗斯', '西班牙', '安道尔', '亞美尼亞', '芬兰', '斯洛伐克', '斯洛文尼亚', '冰岛', '梵蒂冈', '德国', '俄罗斯', '拉脫維亞',
+                 '立陶宛', '蒙特內哥羅', '比利时', '法国', '保加利亚', '波兰', '波斯尼亚和黑塞哥维那']
+    gdpr_list = ['匈牙利', '希腊', '意大利', '列支敦斯登', '挪威', '爱尔兰岛', '爱沙尼亚', '丹麦', '克罗地亚', '羅馬尼亞', '瑞典', '瑞士', '賽普勒斯', '葡萄牙',
+                 '卢森堡', '奥地利', '马耳他', '捷克', '荷兰', '西班牙', '芬兰', '斯洛伐克', '斯洛文尼亚', '冰岛', '德国', '拉脫維亞', '立陶宛', '比利时',
+                 '法国', '保加利亚', '波兰', '英国']
+    check_show_list = []
+    check_gdpr_list = []
     for n in range(len(foreign_data)):
         it = foreign_data[n]
         _it= chinese_data[n]
         it['note'] = _it['name']
-        org = it.get('organization') # type: list[str]
-        it['show'] = 1 if org and 'European Union' in org else 0
-        it['gdpr'] = it['show']
+        it['show'] = 1 if it['note'] in show_list else 0
+        it['gdpr'] = 1 if it['note'] in gdpr_list else 0
+        if it['show']: check_show_list.append(it['note'])
+        if it['gdpr']: check_gdpr_list.append(it['note'])
+    check_show_list.sort()
+    check_gdpr_list.sort()
+    if len(check_show_list) != len(show_list): dump_missing(item_list=show_list, check_list=check_show_list)
+    if len(check_gdpr_list) != len(gdpr_list): dump_missing(item_list=gdpr_list, check_list=check_gdpr_list)
     book = xlwt.Workbook()
     sheet = book.add_sheet(sheetname='COUNTRY_CONF')
     field_names = ('id', 'name', 'show', 'gdpr', 'abbr', 'note', 'continent', 'organization')
